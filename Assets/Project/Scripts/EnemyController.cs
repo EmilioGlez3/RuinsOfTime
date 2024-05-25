b6d1c52d70f34d51d99683fccb6bfc2a5adf8663
EmilioGlez3 <hemgg123@gmail.com>
2024-05-25 11:55:28 -0600

    PATROL y CHASE
    No está pulido al 100%

--- Assets/Project/Scripts/EnemyController.cs
+++ Assets/Project/Scripts/EnemyController.cs
@@ -12,6 +12,11 @@ public class EnemyController : MonoBehaviour
     public EnemyState currentState;
     public Vector2 patrolArea;
     private Vector3 destinationPoint;
+
+    //Cosecha
+    public GameObject door;
+
+
     void Start()
     {
         enemyAnimator = GetComponent<Animator>();
@@ -24,17 +29,27 @@ public class EnemyController : MonoBehaviour
 
     public void GeneratePatrolPosition()
     {
-        if(currentState != EnemyState.PATROL)
-        {
-            return;
-        }
-
         destinationPoint = transform.position + (Vector3.right * Random.Range(-patrolArea.x, patrolArea.x)) + (Vector3.forward * Random.Range(-patrolArea.y, patrolArea.y));
     }
 
     void Update()
     {
-        agent.SetDestination(destinationPoint);
+        if (door.activeInHierarchy == false)
+        {
+            if (currentState != EnemyState.CHASE)
+            {
+                currentState = EnemyState.CHASE; ;
+            }
+        }
+
+        if (currentState == EnemyState.PATROL)
+        {
+            agent.SetDestination(destinationPoint);
+        }
+        else if (currentState == EnemyState.CHASE)
+        {
+            agent.SetDestination(transformPlayer.position);
+        }
         enemyAnimator.SetFloat("Speed", agent.velocity.sqrMagnitude);
     }
 }
